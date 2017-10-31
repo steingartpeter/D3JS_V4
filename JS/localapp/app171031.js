49f1c3ea8fa9cb6eeb47743fb3f03330787641d8
steingartpeter <steingart.p2@gmail.com>
2017-10-31 14:57:52 +0100

    2017-10-31 INIT002

---
+++ JS/localapp/app171031.js
@@ -0,0 +1,29 @@
+//<M>
+//×-
+//@-FILENÉV   : D3JS-V4 - app171031.js-@
+//@-SZERZŐ    : AX07057-@
+//@-LÉTREHOZVA: 2017. okt. 31.-@
+//@-FÜGGŐSÉGEK:
+//×-
+// @-- jQuery.min.js-@
+// @-- bootstrap.min.js-@
+// @-- d3.min.js-@
+// @-- ...-@
+//-@
+//-×
+//-@
+//@-LEÍRÁS    :
+//Ez az javascript file készült arra a feladatra, hogy otthont adjon a mAIN app-nak.
+//@-MÓDOSÍTÁSOK :
+//×-
+// @-- ... -@
+//-×
+//-×
+//</M>
+
+APP171031 = APP171031 || {};
+
+
+$(function(){
+	alert "JQuery - OK";
+})
